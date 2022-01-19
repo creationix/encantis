f4a1f53fac6250b277068e444a26ab250e0f16ad
Tim Caswell <tim@creationix.com>
2022-01-19 01:31:58 -0600

    Implement most of AST parser

--- tools/encantis-format.js
+++ tools/encantis-format.js
@@ -1,26 +1,29 @@
 #!/usr/bin/env node
-import { readFileSync } from "fs";
+import { readFileSync } from "fs"
 import { inspect } from "util"
 
-const BUILTIN_TYPE_PATTERN = /^(u8|i8|u16|i16|u32|i32|u64|i64)\b/;
-const NAME_PATTERN = /^[a-z][a-z0-9_]*/i;
-const HEX_NUMBER_PATTERN = /^0x[0-9a-f]+/i;
-const OCT_NUMBER_PATTERN = /^0o[0-7]+/i;
-const BIN_NUMBER_PATTERN = /^0b[0-1]+/i;
-const DEC_NUMBER_PATTERN = /^[0-9]+/i;
+const BUILTIN_TYPE_PATTERN = /^(u8|i8|u16|i16|u32|i32|u64|i64)\b/
+const NAME_PATTERN = /^[a-z][a-z0-9_-]*/i
+const HEX_NUMBER_PATTERN = /^0x[0-9a-f]+/i
+const OCT_NUMBER_PATTERN = /^0o[0-7]+/i
+const BIN_NUMBER_PATTERN = /^0b[0-1]+/i
+const DEC_NUMBER_PATTERN = /^[0-9]+/i
 
-const STRING_PATTERN = /^"[^"]*"/;
+const STRING_PATTERN = /^"[^"]*"/
 
 const PUNCTUATION = toPattern(
-    "( ) [ ] : = -> . , ; " +
+    "( ) [ ] : = -> . , ; .* " +
     "+ - * / % " +
     "+= -= *= /= %= " +
     "<= >= == != < > " +
-    "<< >> | & ^ " +
-    "<<= >>= |= &= ^= ")
+    "&& || ^^ ! " +
+    "<<< >>> << >> | & ^ " +
+    "<<<= >>>= <<= >>= |= &= ^= ")
 
 const KEYWORDS = toPattern(
-    "export func local forever return when end", "\\b"
+    "and or xor not " +
+    "export func memory global local forever return when end " +
+    "void as if then elif else while do block loop br", "(?![a-zA-Z0-9_-])"
 )
 
 /** @type {[RegExp,boolean|string][]} */
@@ -36,167 +39,233 @@ const patterns = [
     [OCT_NUMBER_PATTERN, 'NUMBER'],
     [BIN_NUMBER_PATTERN, 'NUMBER'],
     [DEC_NUMBER_PATTERN, 'NUMBER'],
-];
+]
 
 function Type(tokens, offset) {
-    return Type.parser(tokens, offset);
+    return Type.parser(tokens, offset)
 }
 Type.parser = choice(
-    P(liven("BUILTINTYPE"), T => ({ builtin: true, offset: T.offset })),
-    P(sequence("[", Type, "]"), T => ({ slice: T[1], offset: T[0].offset })),
-);
-
-
-const E1 = choice(
-    P(liven("NUMBER"), T => ({ numberOffset: T.offset })),
-    P(sequence("-", Expression), T => ({ neg: T[1] })),
-    P(sequence("NAME", optional(choice(
-        P(sequence("(", star(Expression, ","), ")"), T => ({
-            call: T[1],
-        })),
-        P(sequence("[", Expression, "]"), T => ({
-            index: T[1],
-        })),
-        P(sequence(".", "NAME"), T => ({
-            index: { nameOffset: T[1].offset },
-        }))
-    ))), T => (
-        T[1] ? (
-            T[1].call ? {
-                identOffset: T[0].offset,
-                call: T[1].call,
-            } : T[1].index ? {
-                identOffset: T[0].offset,
-                index: T[1].index,
-            } : T[1]
-        ) : {
-            identOffset: T[0].offset
-        }
-    )),
-    P(sequence("(", Expression, ")"), T => T[1]),
+    P(liven("BUILTINTYPE"), T => ({ builtinOffset: T.offset })),
+    P(sequence("(", star(Type), ")"), T => T),
+    P(sequence("[", Type, "]"), T => ({ slice: T[1] })),
+    P(sequence("*", Type), T => ({ pointer: T[1] })),
 )
 
-function binop(lower, op, top) {
-    return P(sequence(lower, optional(sequence(op, top))), T => (
-        T[1] ? {
-            binop: T[1][0].token,
-            left: T[0],
-            right: T[1][1],
-        } : T[0]
+function typable(parser) {
+    return P(sequence(parser, optional(sequence(":", Type))), T => (
+        T[1] ? { ...T[0], type: T[1][1] } : T[0]
     ))
 }
 
-const E2 = binop(E1, choice("<<", ">>", "*", "/", "%"), Expression)
-const E3 = binop(E2, choice("|", "&", "+", "-"), Expression)
-const E4 = binop(E3, choice("<=", ">=", "==", "!=", "<", ">"), Expression)
-
-function Expression(tokens, offset) {
-    return E4(tokens, offset);
+function binop(left, op, right) {
+    return P(sequence(left, star(sequence(op, right))), ([L, S]) =>
+        S.reduce((p, [a, b]) => ([a.token || a, p, b]), L)
+    )
 }
 
-// Declaration = {nameOffset:number,type?:Type}
-const Declaration = P(sequence(
-    "NAME",
-    optional(sequence(":", Type))
-), T => ({
-    nameOffset: T[0].offset,
-    type: T[1] && T[1][1]
-}));
+function prefix(op, lower) {
+    return P(sequence(star(op), lower), ([S, L]) =>
+        S.reduce((p, c) => ([c.token || c, p]), L)
+    )
+}
+
+function postfix(lower, op) {
+    return P(sequence(lower, star(op)), ([L, S]) =>
+        S.reduce((p, c) => ([c.token || c, p]), L)
+    )
+}
+
+const Name = typable(P("NAME", T => ({ nameOffset: T.offset })))
+const Number = typable(P("NUMBER", T => ({ numberOffset: T.offset })))
+
+const Call = P(sequence("(", star(Expression, ","), ")"), T => ({
+    call: T[1],
+}))
+
+
+const Index = choice(
+    P(sequence("[", Expression, "]"), T => ({
+        index: T[1],
+    })),
+    P(sequence(".", choice(Name, Number)), T => ({
+        index: T[1],
+    }))
+)
+const Dereference = ".*"
+
+const E7 = choice(
+    Number,
+    Name,
+    P(sequence("(", Expression, ")"), T => T[1]),
+)
+const E6 = postfix(E7, choice(Call, Index, Dereference))
+const E5 = prefix(choice("-", "!", "~"), E6)
+const E4 = binop(E5, choice("as"), Type)
+const E3 = binop(E4, choice("<<<", ">>>", "<<", ">>", "*", "/", "%", "&"), E4)
+const E2 = binop(E3, choice("|", "+", "-"), E3)
+const E1 = binop(E2, choice("<=", ">=", "==", "!=", "<", ">"), E2)
+
+function Expression(tokens, offset) {
+    return E1(tokens, offset)
+}
+
+const IfThenElse = P(sequence(
+    "if", Expression,
+    "then", star(Statement),
+    star(sequence(
+        "elif", Expression,
+        "then", star(Statement)
+    )),
+    optional(sequence(
+        "else", star(Statement)
+    )),
+    "end"
+), T => {
+    const O = {
+        ifs: [
+            T[1], T[3]
+        ].concat(T[4].map(TT => [TT[1], TT[3]]).flat())
+    }
+    if (T[5]) O.else = T[5][1]
+    return O
+})
 
 const Assignment = P(sequence(
-    "NAME", choice(
+    postfix("NAME", choice(
+        Call,
+        Index,
+        Dereference,
+    )),
+    choice(
         "=",
         "+=", "-=", "*=", "/=", "%=",
-        "<<=", ">>=", "|=", "&=", "^= "
-    ), Expression
+        "<<=", ">>=", "|=", "&=", "^=",
+        "<<<=", ">>>=",
+    ),
+    Expression
 ), T => ({
     assignment: T[1].token,
-    nameOffset: T[0].offset,
+    target: T[0],
     value: T[2],
-    offset: T[1].offset,
 }))
 
-// LocalVariable = {local:Token,type?:Type,value?:Expression}
 const LocalVariable = P(sequence(
-    "local", Declaration, optional(sequence("=", Expression))
-), T => ({
-    local: {
-        nameOffset: T[1].nameOffset,
-        type: T[1].type,
-        value: T[2] && T[2][1],
-    },
-    offset: T[0].offset
-}))
+    "local", Name, optional(sequence("=", Expression))
+), T => {
+    const O = { nameOffset: T[1].nameOffset }
+    if (T[1].type) O.type = T[1].type
+    if (T[2]) O.value = T[2][1]
+    return { local: O }
+})
 
-const Forever = P(sequence(
-    "forever",
+const Loop = P(sequence(
+    choice("forever", "loop", "block"),
     star(Statement, ";"),
     "end"
 ), T => ({
-    forever: T[1],
-    offset: T[0].offset
+    [T[0].token]: T[1],
 }))
 
+const While = P(sequence(
+    "while", Expression, "do",
+    star(Statement, ";"),
+    "end"
+), T => ({ while: [T[1], T[3]] }))
+
+const When = P(sequence(
+    choice("when", "unless"),
+    Expression
+), T => ({ [T[0].token]: T[1] }))
+
+
 const Return = P(sequence(
-    "return", Expression,
-    optional(sequence(
-        choice('when', 'unless'),
-        Expression
-    ))
-), T => (
-    T[2] ? {
-        return: T[1],
-        [T[2][0].token]: T[2][1]
-    } : { return: T[0] }
-))
+    choice("return", "br"), optional(Expression),
+    optional(When)
+), T => {
+    let O = {}
+    if (T[1]) O.value = T[1]
+    if (T[2]) O = { ...O, ...T[2] }
+    return { [T[0].token]: O }
+})
 
 function Statement(tokens, offset) {
     return Statement.parser(tokens, offset)
 }
 Statement.parser = choice(
+    Expression,
     LocalVariable,
     Assignment,
-    Forever,
+    Loop,
+    While,
     Return,
+    IfThenElse,
 )
 
+const MemoryDeclaration = P(sequence(
+    "memory", "NAME", "NUMBER"
+), T => ({
+    memory: {
+        nameOffset: T[1].offset,
+        numberOffset: T[2].offset,
+    }
+}))
 
-const FunctionDeclaration = P(sequence(
-    optional(sequence("export", "STRING")),
-    "func", "NAME", "(", star(Declaration, ","), ")",
-    optional(sequence("->", Type)),
+const GlobalVariable = P(sequence(
+    "global", Name, optional(sequence("=", Expression))
+), T => {
+    const O = T[1]
+    if (T[1].type) O.type = T[1].type
+    if (T[2]) O.value = T[2][1]
+    return { global: O }
+})
+
+const NamedTuple = P(sequence(
+    "(", star(Name, ","), ")"
+), T => T[1])
+
+const Function = P(sequence(
+    "func", optional("NAME"), NamedTuple,
+    optional(sequence("->", choice(NamedTuple, Type))),
     star(Statement, ";"),
     "end"
-), T => ({
-    func: {
-        export: T[0] && T[0][1],
-        nameOffset: T[2].offset,
-        args: T[4],
-        ret: T[6] && T[6][1],
-        body: T[7]
-    },
-    offset: T[1].offset,
-}));
+), T => {
+    const O = {}
+    if (T[1]) O.nameOffset = T[1].offset
+    if (T[2].length) O.args = T[2]
+    if (T[3]) O.ret = T[3][1]
+    if (T[4].length) O.body = T[4]
+    return { func: O }
+})
+
+const Export = P(sequence(
+    "export", "STRING", choice(
+        GlobalVariable,
+        Function
+    )
+), T => ({ exportOffset: T[1].offset, ...T[2] }))
 
 const Program = star(choice(
-    FunctionDeclaration
-), ";");
+    MemoryDeclaration,
+    Function,
+    GlobalVariable,
+    Export
+), ";")
 
 function tokenize(source) {
-    const tokens = [];
-    let offset = 0;
+    const tokens = []
+    let offset = 0
     outer: while (offset < source.length) {
-        const subsource = source.substr(offset);
+        const subsource = source.substr(offset)
         for (const [regex, action] of patterns) {
-            const m = regex.exec(subsource);
+            const m = regex.exec(subsource)
             if (m) {
-                offset += m[0].length;
+                offset += m[0].length
                 if (action) {
                     /** @type {string} */
-                    const token = action === true ? m[0] : action;
-                    tokens.push({ token, offset });
+                    const token = action === true ? m[0] : action
+                    tokens.push({ token, offset })
                 }
-                continue outer;
+                continue outer
             }
         }
         throw new SyntaxError(`while tokenizing @(${offset}) ${JSON.stringify(source.substr(offset, 10))}`)
@@ -208,35 +277,36 @@ function tokenize(source) {
  * @param {{token:string,offset:number}[]} tokens
  */
 function parse(tokens, Start) {
-    const match = Start(tokens, 0);
-    return match && match[1];
+    const match = Start(tokens, 0)
+    return match && match[1]
 }
 
 function P(parser, fn) {
+    parser = liven(parser)
     if (typeof fn !== "function") throw new TypeError("fn must be function")
     return (tokens, offset) => {
-        const match = parser(tokens, offset);
-        if (!match) return false;
+        const match = parser(tokens, offset)
+        if (!match) return false
         const [newoffset, value] = match
-        return [newoffset, fn(value, newoffset)];
-    };
+        return [newoffset, fn(value, newoffset)]
+    }
 }
 
 // Choose the parser with the longest match.
 function choice(...items) {
-    const parsers = items.map(liven);
+    const parsers = items.map(liven)
     return function (tokens, offset) {
-        var longest = 0;
-        var result = null;
+        var longest = 0
+        var result = null
         for (let i = 0, l = parsers.length; i < l; i++) {
-            const match = parsers[i](tokens, offset);
-            if (!match) continue;
-            const [length, node] = match;
-            if (length <= longest) continue;
-            longest = length;
-            result = node;
+            const match = parsers[i](tokens, offset)
+            if (!match) continue
+            const [length, node] = match
+            if (length <= longest) continue
+            longest = length
+            result = node
         }
-        return result && [longest, result];
+        return result && [longest, result]
     }
 }
 
@@ -244,7 +314,7 @@ function choice(...items) {
 function optional(parser) {
     parser = liven(parser)
     if (typeof parser !== "function") throw new TypeError("Parser must be function")
-    return (tokens, offset) => parser(tokens, offset) || [offset];
+    return (tokens, offset) => parser(tokens, offset) || [offset]
 }
 
 // Make a match 0 or more times
@@ -254,91 +324,114 @@ function star(parser, optsep) {
     }
 
     return (tokens, offset) => {
-        const results = [];
+        const results = []
         let match
         while ((match = parser(tokens, offset))) {
-            const [newoffset, value] = match;
+            const [newoffset, value] = match
             if (newoffset > offset) {
-                results.push(value);
-                offset = newoffset;
+                results.push(value)
+                offset = newoffset
             }
         }
-        return [offset, results];
+        return [offset, results]
     }
 }
 
 // Make a match 1 or more times
 function plus(parser, optsep) {
-    parser = star(parser, optsep);
+    parser = star(parser, optsep)
     return (tokens, offset) => {
-        const match = parser(tokens, offset);
-        return match.length ? match : false;
-    };
+        const match = parser(tokens, offset)
+        return match.length ? match : false
+    }
 }
 
 function sequence(...items) {
-    const parsers = items.map(liven);
+    const parsers = items.map(liven)
     return (tokens, offset) => {
-        const list = [];
+        const list = []
         for (let i = 0, l = parsers.length; i < l; i++) {
-            const parser = parsers[i];
-            const match = parser(tokens, offset);
-            if (!match) return false;
-            const [newOffset, value] = match;
-            list.push(value);
-            offset = newOffset;
+            const parser = parsers[i]
+            const match = parser(tokens, offset)
+            if (!match) return false
+            const [newOffset, value] = match
+            list.push(value)
+            offset = newOffset
         }
-        return [offset, list];
+        return [offset, list]
     }
 }
 
 // Turn primitives into name matchers.
 function liven(item) {
-    if (typeof item === "function") return item;
+    if (typeof item === "function") return item
     return (tokens, offset) => {
-        const token = tokens[offset];
-        return (token && token.token === item) ? [offset + 1, token] : false;
-    };
+        const token = tokens[offset]
+        return (token && token.token === item) ? [offset + 1, token] : false
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
 
 const tests = [
-    // ["[u8]", Type],
-    // ["trieOffset: u32", Declaration],
-    // ["trieOffset", Declaration],
-    // ["0", Expression],
-    // ["local trieOffset: u32 = 0", LocalVariable],
-    // ["local trieOffset = 0", LocalVariable],
-    // ["local trieOffset: u32", LocalVariable],
-    // ["local trieOffset", LocalVariable],
-    // ["func nop() forever a=1 b=2 end end", FunctionDeclaration],
-    // ["func add(a b) end", FunctionDeclaration],
-    // ["func add(a:u32 b:u32) end", FunctionDeclaration],
-    // ["func add(a b) -> u32 end", FunctionDeclaration],
-    // ["export \"walk\" func a() end", FunctionDeclaration],
-    // ["1 << 2", Expression],
-    // ["1 << 2 | 5", Expression],
-    // ["1 << ((key >> bitOffset) & 0x7f)", Expression],
-    // ["(1 + 2)", Expression],
-    // ["(bitOffset + 7) % 64", Expression],
-    // ["bitOffset = (bitOffset + 7) % 64", Assignment],
-    // ['local index: u64 = 1 << ((key >> bitOffset) & 0x7f)', Statement],
-    // ['bitOffset = (bitOffset + 7) % 64', Statement],
-    // ['local bitfield: u64 = trie[trieOffset]', Statement],
-    // ['trieOffset += 1', Statement],
-    // ['return -1 when (bitfield & index) == 0)', Statement],
-    // ['trieOffset += popcount(bitfield & (index - 1))', Statement],
-    // ['return -2 when trieOffset >= trie.len', Statement],
-    // ['local pointer: u64 = trie[trieOffset]', Statement],
-    // ['return pointer when (pointer & 0x8000000000000000) == 0', Statement],
-    // ['trieOffset += pointer & 0x7fffffffffffffff', Statement],
-    // ['return -2 when trieOffset >= trie.len', Statement],
-    // ['func no() end', Program]
-];
+    ["[u8]", Type],
+    ["trieOffset: u32", Name],
+    ["trieOffset", Name],
+    ["0", Expression],
+    ["local trieOffset: u32 = 0", LocalVariable],
+    ["local trieOffset = 0", LocalVariable],
+    ["local trieOffset: u32", LocalVariable],
+    ["local trieOffset", LocalVariable],
+    ["func nop() forever a=1 b=2 end end", Function],
+    ["func add(a b) end", Function],
+    ["func add(a:u32 b:u32) end", Function],
+    ["func add(a b) -> u32 end", Function],
+    ["export \"walk\" func a() end", Program],
+    ["1 << 2", Expression],
+    ["1 << 2 | 5", Expression],
+    ["1 << ((key >> bitOffset) & 0x7f)", Expression],
+    ["(1 + 2)", Expression],
+    ["(bitOffset + 7) % 64", Expression],
+    ["bitOffset = (bitOffset + 7) % 64", Assignment],
+    ['local index: u64 = 1 << ((key >> bitOffset) & 0x7f)', Statement],
+    ['bitOffset = (bitOffset + 7) % 64', Statement],
+    ['local bitfield: u64 = trie[trieOffset]', Statement],
+    ['trieOffset += 1', Statement],
+    ['return -1 when (bitfield & index) == 0)', Statement],
+    ['trieOffset += popcount(bitfield & (index - 1))', Statement],
+    ['return -2 when trieOffset >= trie.len', Statement],
+    ['local pointer: u64 = trie[trieOffset]', Statement],
+    ['return pointer when (pointer & 0x8000000000000000) == 0', Statement],
+    ['trieOffset += pointer & 0x7fffffffffffffff', Statement],
+    ['return -2 when trieOffset >= trie.len', Statement],
+    ['func no() end', Program],
+    ['2654435761:u32', Number],
+    ['prime32-1', Expression],
+    ['end-of-line', Expression],
+    ['global prime32-1 = 2654435761:u32', Program],
+    ['memory mem 1', Program],
+    ['func (ptr: *u32, len: u32, seed: u32) -> u32 end', Program],
+    ['export "empty" func () end', Program],
+    ['v1 = round32(v1, ptr.*)', Statement],
+    ['if 1 then 2 elif 3 then 4 else 5 end', Statement],
+    ['while ptr + 4 <= last do true end', Statement],
+    ['foo.bar()', Expression],
+    ['1 + 2 + 3', Expression],
+    ['-Lib.calc().more', Expression],
+    ['a.b + c.d', Expression],
+    ['a * b + c * d', Expression],
+    ['h32 += (ptr as u8).* & prime32-5', Statement],
+    ['seed += value * prime32-2', Statement],
+    ['seed <<<= 13', Statement],
+    ['seed *= prime32-1', Statement],
+    ['return seed', Statement],
+    ['func (ptr: *u64, len: u32) end', Program],
+    ['ptr.* = xxh64(ptr + 8, len, ptr.*)', Assignment],
+    ['ptr:*u8.*', Expression],
+]
 
-if (!tests.length || true) {
+if (!tests.length) {
     const script = process.argv[2]
     if (script) {
         tests.push([readFileSync(script, 'utf8'), Program])
@@ -352,14 +445,14 @@ if (!tests.length || true) {
 for (const [code, Start] of tests) {
     console.log("\n" + inspect(code, { colors: true }))
     const tokens = tokenize(code)
-    // console.log(tokens)
+    console.log(tokens)
     const ast = parse(tokens, Start)
-    console.log(inspect(ast, { colors: true, depth: 8 }));
+    console.log(inspect(ast, { colors: true, depth: 10 }))
     if (!ast) throw new Error("Failed to parse")
 }
 
 function escapeRegExp(string) {
-    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') // $& means the whole matched string
 }
 
 function toPattern(words, extra = "") {
