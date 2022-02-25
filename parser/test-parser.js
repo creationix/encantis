237b267bdab9a1d170cf204c7dc5a0d1eab49011
Tim Caswell <tim@creationix.com>
2022-02-24 22:16:59 -0600

    More parser progress

--- parser/test-parser.js
+++ parser/test-parser.js
@@ -1,13 +1,63 @@
 import { inspect } from "util"
 import peggy from "peggy"
 import { readFileSync } from "fs"
+
+const keywords = [
+    'as',
+    'block',
+    'br',
+    'call',
+    'destructure',
+    'do',
+    'elif',
+    'else',
+    'end',
+    'export',
+    'f32',
+    'f64',
+    'for',
+    'forever',
+    'func',
+    'global',
+    'i16',
+    'i32',
+    'i64',
+    'i8',
+    'if',
+    'in',
+    'index',
+    'local',
+    'loop',
+    'memory',
+    'param',
+    'ptr',
+    'result',
+    'return',
+    'slice',
+    'then',
+    'tuple',
+    'u16',
+    'u32',
+    'u64',
+    'u8',
+    'unless',
+    'var',
+    'when',
+    'while',
+]
+
 const grammarSource = readFileSync('Encantis.peggy', 'utf8')
 const parser = peggy.generate(grammarSource)
-const filename = "../samples/trie.ents"
+const filename = "../samples/gimli.ents"
 const source = readFileSync(filename, 'utf8')
 try {
     const ast = parser.parse(source, { grammarSource: filename })
     console.log(inspect(ast, {
+        depth: 100,
+        colors: true
+    }))
+    const [literals, processed] = process(ast)
+    console.log(inspect({ literals, processed }, {
         depth: 100,
         colors: true
     }))
@@ -22,5 +72,28 @@ try {
         process.exit(-1)
     } else {
         throw err
+    }
+}
+
+
+function process(ast) {
+    const literals = []
+    return [literals, walk(ast)]
+    function walk(node) {
+
+        if (Array.isArray(node)) {
+            return node.map(walk)
+        }
+
+        let index = keywords.indexOf(node)
+        if (index >= 0) return index
+
+        const type = typeof node
+        if (type === "number" || type === "string" && node.length > 3) {
+            index = literals.indexOf(node)
+            if (index >= 0) return index + keywords.length
+            return literals.push(node) - 1
+        }
+        return node
     }
 }
