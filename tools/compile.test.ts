ab2826ebb8a39375155503f2008518311225cc3c
Tim Caswell <creationix@vercel.com>
2024-06-07 10:35:38 -0700

    Start compiler

---
+++ tools/compile.test.ts
@@ -0,0 +1,5 @@
+import { compile } from "./compile"
+import { expect, test } from 'bun:test'
+
+test('compiles a basic module', () => {
+})
---
+++ tools/compile.ts
@@ -0,0 +1,4 @@
+// Given a string containing encantis source code, return a string containing wat source code.
+export function compile(src: string): string {
+
+}
