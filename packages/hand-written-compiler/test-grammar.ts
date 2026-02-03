ac5a8dcd019f9feb74c36de9d2e21dbf16a08e70
Tim Caswell <tim@creationix.com>
2026-02-03 16:56:25 -0600

    Setting up test system

--- packages/hand-written-compiler/test-grammar.ts
+++ packages/hand-written-compiler/test-grammar.ts
@@ -1,9 +1,13 @@
 import * as ohm from 'ohm-js'
 
-const script = await Bun.file(new URL('./grammar.ohm', import.meta.url)).text()
+const markdownTestGrammar = ohm.grammar(await Bun.file(new URL('./markdown-tests.ohm', import.meta.url)).text())
+const encantisGrammar = ohm.grammar(await Bun.file(new URL('./encantis.ohm', import.meta.url)).text())
 
-const grammar = ohm.grammar(script)
+console.log('Grammars loaded successfully.')
 
-console.log('Grammar loaded successfully.')
+console.log({ markdownTestGrammar, encantisGrammar })
 
-console.log({ grammar })
+const testsMd = await Bun.file(new URL('./parse-tests.md', import.meta.url)).text()
+
+console.log({ testsMd })
+
